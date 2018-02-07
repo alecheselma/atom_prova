9eeeebea9565e0f52c8655be10523a3353be897e
alecheselma <alecheselma@gmail.com>
2018-02-07 11:36:49 +0100

    Primer intent

---
+++ prueva.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$directorio = 'animales';
+$ficheros1  = scandir($directorio);
+
+unset($ficheros1[0]);
+echo("holad45ijd");
+
+echo("hola");
+
+unset($ficheros1[1]);
+
+
+foreach ($ficheros1 as &$valor) {
+    echo"<img src=".$valor."> <p>$valor</p>";}
+
+
+    echo("holad45ijd");
+      echo("holad45ijd");
+        echo("holad45ijd");
+
+    echo("holad45ijd");
+
+?>
+
+</body>
+</html>
